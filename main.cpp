5e814dc1c92a841bc441fa4de92d340fb3284deb
Fybii <Fabian.Peifer23@gmail.com>
2024-03-01 18:04:53 +0100

    Help txt und Konsoleneingabe mit getopt

--- main.cpp
+++ main.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
-#include <helptext.h>
+#include <getopt.h>
+//including of the help file that will be displayed
+#include "header/helptext.h"
 
-int main(int argc, char const *argv[])
+//function to put the help text in the console
+void help() {
+    puts(HELPTEXT);
+    exit(0);
+}
+
+//longopts structure which is used to use long operators as commands
+struct option longopts[] = {
+    //input: help, no argument is needed and safe as char 'h';
+    {"help", no_argument, NULL, 'h'},
+    {0, 0, 0, 0}
+};
+
+int main(int argc, char *argv[])
 {
-
-    if (argc < 2) {
-        
+    //only start programm if user input is available 
+    if(argc < 2) {
         std::cerr << "Error\n";
-        
     }
-    else
-    {
-        std::cout << "Hallo\n" << argv[0];
+    else {
+        //Set variables for the option char and index
+        int option;
+        int index;
+        //Loop for checking each command that is given by the user
+        while ((option = getopt_long(argc, argv, "h", longopts, &index)) != -1) {
+            std::cout << option << "\n";
+            switch (option)
+            {
+            case 'h':
+                help();
+                break;
+            
+            default:
+                std::cerr << "invalid Input\n";
+                break;
+            };
+        };
     }
     return 0;
 }
